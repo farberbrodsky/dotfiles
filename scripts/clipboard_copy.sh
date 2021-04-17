33f304715142d219a31dd2d34286a82ecfd55870
Misha Farber Brodsky <misha@farberbrodsky.com>
2021-04-17 17:55:59 +0300

    Update timings to make it work with lyx

--- scripts/clipboard_copy.sh
+++ scripts/clipboard_copy.sh
@@ -1,9 +1,9 @@
 TEMP=$(xclip -sel clip -o)
 xdotool keyup $1
-sleep 0.01
+sleep 0.02
 
 xdotool key --clearmodifiers ctrl+c
-sleep 0.01
+sleep 0.02
 
 xclip -sel clip -o > /tmp/clipboard-$1
 
--- scripts/clipboard_paste.sh
+++ scripts/clipboard_paste.sh
@@ -1,9 +1,9 @@
 TEMP=$(xclip -sel clip -o)
 xdotool keyup $1
 xclip -sel clip -i < /tmp/clipboard-$1
-sleep 0.01
+sleep 0.03
 
 xdotool key ctrl+v
-sleep 0.01
+sleep 0.1
 
 echo $TEMP | xclip -sel clip -i
